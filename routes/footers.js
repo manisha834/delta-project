7e642263e4126279200e64ec50adbeda3201819b
manisha834 <bhattamanisha845@gmail.com>
2025-09-29 15:44:30 +0330

    added privacy file

--- routes/footers.js
+++ routes/footers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const footers = require("../controllers/pages");
+const footers = require("../controllers/footers");
 
 
 //privacy route
