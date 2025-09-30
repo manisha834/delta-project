f8aa9d80323c2748db2c5ffa706e18421af44f25
manisha834 <bhattamanisha845@gmail.com>
2025-09-30 06:51:05 +0330

    adding changes

--- controllers/footers.js
+++ controllers/footers.js
@@ -1,3 +1,6 @@
 module.exports.renderPrivacy = (req,res) => {  
     res.render("listings/privacy");
 };
+module.exports.renderTerms = (req,res) => {  
+    res.render("listings/terms");
+};
--- routes/footers.js
+++ routes/footers.js
@@ -6,4 +6,6 @@ const footers = require("../controllers/footers.js");
 //privacy route
 router.get("/privacy",footers.renderPrivacy);
 
+router.get("/terms",footers.renderTerms);
+
 module.exports = router;
